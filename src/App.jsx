68b8cc1b9fd4f3640bddcf6c2e54e1da85ad704e
Dulara J <dularaj8@gmail.com>
2026-05-10 20:00:08 +0530

    feat: update App component to include Skills, Projects, and Contact sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Navbar from './components/navbar.jsx'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Skills from './pages/Skills.jsx'
+import Projects from './pages/Projects.jsx'
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   )
 }

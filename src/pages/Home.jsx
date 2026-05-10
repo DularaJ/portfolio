acdbfabc76fe3198538b760062273b180bb4c2b3
Dulara J <dularaj8@gmail.com>
2026-05-10 19:29:48 +0530

    feat: create Home component for personal introduction

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <section className="home">
+      <h1>Hi, I'm Dulara Jayasanka</h1>
+
+      <p>
+        ICT Undergraduate | Software Engineering Enthusiast
+      </p>
+
+      <button>View Projects</button>
+    </section>
+  )
+}
+
+export default Home

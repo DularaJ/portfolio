b3da7fa898af649409eb396b7221fc57bd212eaf
Dulara J <dularaj8@gmail.com>
2026-05-10 19:39:02 +0530

    feat: add About component for personal introduction

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+
+      <p>
+        I am a 3rd year ICT undergraduate student specializing in software engineering.
+      </p>
+    </section>
+  )
+}
+
+export default About
